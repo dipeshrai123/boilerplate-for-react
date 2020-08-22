4800128ce52ddea738c652da2bfe94d656710c59
raidipesh78 <raidipesh78@gmail.com>
2020-08-22 15:15:00 +0545

    Progress action set added

--- src/helpers/ActionSet.helper.js
+++ src/helpers/ActionSet.helper.js
@@ -1,9 +1,13 @@
-// CREATE THE ACTION SET
-export function createActionSet(actionName) {
-  return {
+export function createActionSet(actionName, hasProgress = false) {
+  const actionObj = {
     LOADING: `${actionName}_LOADING`,
     SUCCESS: `${actionName}_SUCCESS`,
     ERROR: `${actionName}_ERROR`,
-    actionName
   };
+
+  if (hasProgress) {
+    actionObj.PROGRESS = `${actionName}_PROGRESS`;
+  }
+
+  return actionObj;
 }
